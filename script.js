5072aae621316d754e2c47397c46a55eabd64a71
Isabellac1005 <isabellac1005@nycstudents.net>
2026-01-09 14:35:55 -0500

    dom-mini-pr-02-Isabellac1005

--- script.js
+++ script.js
@@ -7,31 +7,42 @@ let nebula_spot = 1;
 let comet_spot  = 1;
 
 // Query Selectors
-const apolloBtn   = 
-const nebulaBtn   = 
-const cometBtn    = 
+const apolloBtn   = document.querySelector("#apollo-Btn");
+const nebulaBtn   = document.querySelector("#nebula-Btn");
+const cometBtn    = document.querySelector("#comet-Btn");
 
-const apolloRocket = 
-const nebulaRocket = 
-const cometRocket  = 
+const apolloRocket = document.querySelector("#apollo-Rocket");
+const nebulaRocket = document.querySelector("#nebula-Rocket");
+const cometRocket  = document.querySelector("#comet-Rocket")
 
-const winnerBox = 
+const winnerBox = document.querySelector("#winner-Box");
 
 // Click Handlers
 const boostApollo = () => {
-  
+  shoppingCart.innerHTML += `<p> Apollo</p>`;
+  totalInCent += 67;
+  updateTotal();
 };
 
 const boostNebula = () => {
-
+shoppingCart.innerHTML += `<p> Nebula </p>`;
+totalInCent += 76;
+updateTotal();
 };
 
 const boostComet = () => {
-
+shoppingCart.innerHTML += `<p> Comet </p>`;
 };
 
 // Winner Check
+const checkWinner = (position,color) => { 
 
+}
 
+if (position === 5) {
+    winnerText.innerHTML = color + "is the Winner!";
+}
 // Events
-
+apolloBtn.addEventListener("click", advanceApolloBtn);
+nebulaBtn.addEventListener("click", advanceNebulaBtn);
+cometRocket.addEventListener("click", advanceCometRocket);
